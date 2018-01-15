caf13ee5f073c3160ea9b5789d9c095f55cbd1b3
warlyware <danward@gmail.com>
2018-01-15 17:40:49 -0500

    Add remaining styles for list-items example

--- chapter-1/list-items/App.js
+++ chapter-1/list-items/App.js
@@ -76,4 +76,45 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     paddingBottom: 20,
   },
+  iconContainer: {
+    alignItems: 'center',
+    backgroundColor: '#feb401',
+    borderColor: '#feaf12',
+    borderRadius: 25,
+    borderWidth: 1,
+    justifyContent: 'center',
+    height: 50,
+    width: 50,
+  },
+  icon: {
+    tintColor: '#fff',
+    height: 22,
+    width: 22,
+  },
+  info: {
+    flex: 1,
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  items: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  address: {
+    color: '#ccc',
+    fontSize: 14,
+  },
+  total: {
+    width: 80,
+  },
+  date: {
+    fontSize: 12,
+    marginBottom: 5,
+  },
+  price: {
+    color: '#1cad61',
+    fontSize: 25,
+    fontWeight: 'bold',
+  }
 });
