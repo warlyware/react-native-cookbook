866a2c65e6faec54654e7374d0af7a97c890ca1e
warlyware <danward@gmail.com>
2018-01-14 21:04:05 -0500

    Add flower image for fake video, fix layout

--- chapter-1/fake-video-player/App.js
+++ chapter-1/fake-video-player/App.js
@@ -1,21 +1,76 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image, ImageBackground } from 'react-native';
+
+const playIcon = require('./images/play.png');
+const volumeIcon = require('./images/sound.png');
+const hdIcon = require('./images/hd-sign.png');
+const fullScreenIcon = require('./images/full-screen.png');
+const flower = require('./images/flower.jpg');
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.appContainer}>
+        <ImageBackground source={flower} style={styles.videoContainer} resizeMode="contain">
+          <View style={styles.controlsContainer}>
+            <Image source={volumeIcon} style={styles.icon} />
+            <View style={styles.progress}>
+              <View style={styles.progressBar} />
+            </View>
+            <Image source={hdIcon} style={styles.icon} />
+            <Image source={fullScreenIcon} style={styles.icon} />
+          </View>
+        </ImageBackground>
       </View>
     );
   }
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  flower: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  appContainer: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  videoContainer: {
+    backgroundColor: '#000',
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  controlsContainer: {
+    padding: 10,
+    backgroundColor: '#202020',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 175,
+  },
+  icon: {
+    tintColor: '#fff',
+    height: 16,
+    width: 16,
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  progress: {
+    backgroundColor: '#000',
+    borderRadius: 7,
+    flex: 1,
+    height: 14,
+    margin: 4,
+  },
+  progressBar: {
+    backgroundColor: '#bf161c',
+    borderRadius: 5,
+    height: 10,
+    margin: 2,
+    paddingTop: 3,
+    width: 80,
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
