7c00282fc51b6645b8745431fa78e0e960957dbf
warlyware <danward@gmail.com>
2018-01-21 16:27:23 -0500

    Add basic App.js component

--- chapter-3/tablet-flexbox/App.js
+++ chapter-3/tablet-flexbox/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { ListView, StyleSheet, Text, View } from 'react-native';
+
+import Post from './Post';
+import data from './data.json';
+
+const dataSouce = new ListView.DataSource({
+  rowHasChanged: (r1, r2) => r1 !== r2,
+});
 
 export default class App extends React.Component {
+  state = {
+    dataSource: dataSouce.cloneWithRows(data.posts),
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.toolbar}>
+          <Text style={styles.title}>Latest posts</Text>
+        </View>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={post => <Post {...post} />}
+          style={styles.list}
+          contentContainerStyle={styles.content}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // Defined on step 13
 });
