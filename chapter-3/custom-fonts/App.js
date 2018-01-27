5f3f7216fff6caadf65d15557d4bf14dc8647c3a
warlyware <danward@gmail.com>
2018-01-27 14:28:16 -0500

    Remove unused Roboto fonts, add code to await loaded font, render font when loaded

--- chapter-3/custom-fonts/App.js
+++ chapter-3/custom-fonts/App.js
@@ -4,19 +4,28 @@ import { Text, View, StyleSheet } from 'react-native';
 import { Font } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
 
-  componentDidMount() {
-    Font.loadAsync({
-      'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+  async componentDidMount() {
+    await Font.loadAsync({
+      'josefin-sans-regular': require('./assets/fonts/JosefinSans-Regular.ttf'),
     });
+
+    this.setState({ fontLoaded: true });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.roboto}>
-          Hello, Roboto!
-        </Text>
+          {
+            this.state.fontLoaded ? (
+              <Text style={styles.josefineSans}>
+                Hello, Josefin Sans!
+              </Text>
+            ) : null
+          }
       </View>
     );
   }
@@ -29,8 +38,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  roboto: {
+  josefineSans: {
     fontSize: 40,
-    fontFamily: 'roboto-regular'
+    fontFamily: 'josefin-sans-regular'
   }
 });
