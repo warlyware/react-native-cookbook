3a101fe3a6cf66030d63ea0cd47c071934acc889
warlyware <danward@gmail.com>
2018-01-29 21:59:10 -0500

    Add FontAwesome and Ionicon iicons

--- chapter-3/font-icons/App.js
+++ chapter-3/font-icons/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.iconRow}>
+          <FontAwesome style={styles.iconPadding} name="glass" size={48} color="green" />
+          <FontAwesome style={styles.iconPadding} name="beer" size={48} color="red" />
+          <FontAwesome style={styles.iconPadding} name="music" size={48} color="blue" />
+          <FontAwesome style={styles.iconPadding} name="taxi" size={48} color="#1CB5AD" />
+        </View>
+        <View style={styles.iconRow}>
+          <Ionicons style={styles.iconPadding} name="md-pizza" size={48} color="orange" />
+          <Ionicons style={styles.iconPadding} name="md-tennisball" size={48} color="maroon" />
+          <Ionicons style={styles.iconPadding} name="ios-thunderstorm-outline" size={48} color="purple" />
+          <Ionicons style={styles.iconPadding} name="ios-happy-outline" size={48} color="#DF7977" />
+        </View>
       </View>
     );
   }
@@ -18,4 +30,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  iconRow: {
+    flexDirection: 'row',
+  },
+  iconPadding: {
+    padding: 8,
+  }
 });
