23031bd05aef944bf4d2c0c35d1fb306886832c3
warlyware <danward@gmail.com>
2018-02-10 14:16:12 -0500

    Add basic working recipe

--- chapter-4/web-view/App.js
+++ chapter-4/web-view/App.js
@@ -1,21 +1,116 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  WebView,
+} from 'react-native';
 
-export default class App extends React.Component {
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+class HomeScreen extends Component {
+  state = {
+    links: [
+      {
+        title: 'Gitconnected Blog',
+        url: 'https://medium.com/gitconnected'
+      },
+      {
+        title: 'Smashing Magazine',
+        url: 'https://www.smashingmagazine.com/articles/'
+      },
+      {
+        title: 'CSS Tricks',
+        url: 'https://css-tricks.com/'
+      },
+      {
+        title: 'Codrops',
+        url: 'https://tympanus.net/codrops/'
+      }
+     ],
+  };
+
+  handleButtonPress(btn) {
+    console.log(this.props, url);
+    const { url, title } = btn;
+    this.props.navigation.navigate('Browser', { url, title });
+  }
+
+  renderButton = (btn, index) => {
+    return (
+      <TouchableOpacity
+        key={index}
+        onPress={() => this.handleButtonPress(btn)}
+        style={styles.btn}
+      >
+        <Text style={styles.text}>{btn.title}</Text>
+      </TouchableOpacity>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        {this.state.links.map(this.renderButton)}
+      </SafeAreaView>
     );
   }
 }
 
+class BrowserScreen extends Component {
+  render() {
+    const { params } = this.props.navigation.state;
+
+    return(
+      <WebView
+        source={{uri: params.url}}
+      />
+    );
+  }
+}
+
+const App = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Home'
+    }),
+  },
+  Browser: {
+    screen: BrowserScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: navigation.state.params.title
+    }),
+  },
+});
+
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    fontSize: 30
+  },
+  btn: {
+    margin: 10,
+    backgroundColor: '#c0392b',
+    borderRadius: 3,
+    padding: 10,
+    paddingRight: 30,
+    paddingLeft: 30,
+  },
+  text: {
+    color: '#fff',
+    textAlign: 'center',
   },
 });
