0149ce5767967730438f2a6bd312aa23e7492d26
warlyware <danward@gmail.com>
2018-02-19 17:09:22 -0500

    Add working recipe

--- chapter-5/multiple-animations/App.js
+++ chapter-5/multiple-animations/App.js
@@ -1,21 +1,101 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+  Animated,
+  Image,
+  Easing,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
 
-export default class App extends React.Component {
+const { width, height } = Dimensions.get('window');
+const cloudImage1 = require('./assets/images/cloud.png');
+const cloudImage2 = require('./assets/images/cloud.png');
+const planeImage = require('./assets/images/plane.gif');
+const cloudHeight = 100;
+const cloudWidth = 150;
+const planeHeight = 60;
+const planeWidth = 100;
+
+export default class App extends Component {
+  componentWillMount() {
+    this.animatedValue = new Animated.Value();
+  }
+
+  componentDidMount() {
+    this.startAnimation();
+  }
+
+  startAnimation () {
+    this.animatedValue.setValue(1);
+    Animated.timing(
+      this.animatedValue,
+      {
+        toValue: 0,
+        duration: 6000,
+        easing: Easing.linear
+      }
+    ).start(() => this.startAnimation());
+  }
+
   render() {
+    const left1 = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-cloudWidth, width],
+    });
+
+    const left2 = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-cloudWidth*5, width + cloudWidth*5],
+    });
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.background}>
+        <Animated.Image
+          style={[
+            styles.cloud1,
+            { left: left1 },
+          ]}
+          source={cloudImage1}
+        />
+        <Image
+          style={styles.plane}
+          source={planeImage}
+        />
+        <Animated.Image
+          style={[
+            styles.cloud2,
+            { left: left2 },
+          ]}
+          source={cloudImage2}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'cyan',
   },
+  cloud1: {
+    position: 'absolute',
+    width: cloudWidth,
+    height: cloudHeight,
+    top: height / 3 - cloudWidth / 2,
+  },
+  cloud2: {
+    position: 'absolute',
+    width: cloudWidth * 1.5,
+    height: cloudHeight * 1.5,
+    top: height/2,
+  },
+  plane: {
+    position: 'absolute',
+    height: planeHeight,
+    width: planeWidth,
+    top: height / 2 - planeHeight,
+    left: width / 2 - planeWidth,
+  }
 });
