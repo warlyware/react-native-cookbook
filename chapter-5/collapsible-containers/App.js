8a4986aa33760163e40a9e39daa67be35a7434fa
warlyware <danward@gmail.com>
2018-03-17 15:31:05 -0400

    Add animations to android via UIManager

--- chapter-5/collapsible-containers/App.js
+++ chapter-5/collapsible-containers/App.js
@@ -1,49 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Text,
   StyleSheet,
   View,
   SafeAreaView,
+  Platform,
+  UIManager
 } from 'react-native';
 import Panel from './Panel';
 
-const App = () => (
-  <SafeAreaView style={styles.main}>
-    <Text style={styles.toolbar}>Animated containers</Text>
-    <View style={styles.content}>
-      <Panel
-        title={'Container 1'}
-        style={styles.panel}
-      >
-        <Text style={styles.panelText}>
-          Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe
-          eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
-        </Text>
-      </Panel>
-      <Panel
-        title={'Container 2'}
-        style={styles.panel}
+
+export default class App extends Component {
+  componentWillMount() {
+    if (Platform.OS === 'android') {
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={[styles.main]}>
+        <Text style={styles.toolbar}>Animated containers</Text>
+        <View style={styles.content}>
+          <Panel
+            title={'Container 1'}
+            style={styles.panel}
           >
-        <Text style={styles.panelText}>
-          Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore,
-          cum soluta nobis est eligendi optio cumque.
-        </Text>
-      </Panel>
-      <Panel
-        expanded
-        title={'Container 3'}
-        style={styles.panel}
-       >
-        <Text style={styles.panelText}>
-          Nullam lobortis eu lorem ut vulputate.
-        </Text>
-        <Text style={styles.panelText}>
-          Donec id elementum orci. Donec fringilla lobortis ipsum, vitae commodo urna.
-        </Text>
-      </Panel>
-    </View>
-  </SafeAreaView>
-);
+            <Text style={styles.panelText}>
+              Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+              eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+            </Text>
+          </Panel>
+          <Panel
+            title={'Container 2'}
+            style={styles.panel}
+              >
+            <Text style={styles.panelText}>
+              Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore,
+              cum soluta nobis est eligendi optio cumque.
+            </Text>
+          </Panel>
+          <Panel
+            expanded
+            title={'Container 3'}
+            style={styles.panel}
+           >
+            <Text style={styles.panelText}>
+              Nullam lobortis eu lorem ut vulputate.
+            </Text>
+            <Text style={styles.panelText}>
+              Donec id elementum orci. Donec fringilla lobortis ipsum, vitae commodo urna.
+            </Text>
+          </Panel>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   main: {
@@ -68,5 +81,3 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
   }
 });
-
-export default App;
