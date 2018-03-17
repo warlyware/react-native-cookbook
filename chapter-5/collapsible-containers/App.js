d37fad51e12246730594a616f48fca689e9b1e50
warlyware <danward@gmail.com>
2018-03-17 15:17:59 -0400

    Build out working recipe

--- chapter-5/collapsible-containers/App.js
+++ chapter-5/collapsible-containers/App.js
@@ -1,21 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  View,
+  SafeAreaView,
+} from 'react-native';
+import Panel from './Panel';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+const App = () => (
+  <SafeAreaView style={styles.main}>
+    <Text style={styles.toolbar}>Animated containers</Text>
+    <View style={styles.content}>
+      <Panel
+        title={'Container 1'}
+        style={styles.panel}
+      >
+        <Text style={styles.panelText}>
+          Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+          eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+        </Text>
+      </Panel>
+      <Panel
+        title={'Container 2'}
+        style={styles.panel}
+          >
+        <Text style={styles.panelText}>
+          Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore,
+          cum soluta nobis est eligendi optio cumque.
+        </Text>
+      </Panel>
+      <Panel
+        expanded
+        title={'Container 3'}
+        style={styles.panel}
+       >
+        <Text style={styles.panelText}>
+          Nullam lobortis eu lorem ut vulputate.
+        </Text>
+        <Text style={styles.panelText}>
+          Donec id elementum orci. Donec fringilla lobortis ipsum, vitae commodo urna.
+        </Text>
+      </Panel>
+    </View>
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  toolbar: {
+    backgroundColor: '#3498db',
+    color: '#fff',
+    fontSize: 22,
+    padding: 20,
+    textAlign: 'center',
+  },
+  content: {
+    padding: 10,
+    backgroundColor: '#ecf0f1',
+    flex: 1,
+  },
+  panel: {
+    marginBottom: 10,
+  },
+  panelText: {
+    paddingBottom: 15,
+  }
 });
+
+export default App;
---
+++ chapter-5/collapsible-containers/Panel/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import {
+  View,
+  LayoutAnimation,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class Panel extends Component {
+  state = {
+    height: this.props.expanded ? null : 0,
+  };
+
+  static defaultProps = {
+    expanded: false,
+    onCollapse: () => {},
+    onExpand: () => {},
+  };
+
+  onToggle = () => {
+    LayoutAnimation.spring();
+    this.setState({
+      height: this.state.height === null ? 0 : null,
+    })
+  }
+
+  render() {
+    const { children, style, title } = this.props;
+    const { height } = this.state;
+
+    return (
+      <View style={[styles.main, style]}>
+        <TouchableOpacity onPress={this.onToggle}>
+          <Text style={styles.title}>
+            {title}
+          </Text>
+        </TouchableOpacity>
+        <View style={{ height }}>
+          {children}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: '#fff',
+    borderRadius: 3,
+    overflow: 'hidden',
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  title: {
+    fontWeight: 'bold',
+    paddingTop: 15,
+    paddingBottom: 15,
+  }
+});
