2d01c55bfaad99ab37bfa8472c0e783ca34b173b
warlyware <danward@gmail.com>
2018-04-07 16:48:46 -0400

    Add working recipe

--- chapter-7/remote-api/App.js
+++ chapter-7/remote-api/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    results: '',
+  };
+
+  onLoad = async () => {
+    this.setState({ results: 'Loading, please wait...' });
+    const response = await fetch('http://jsonplaceholder.typicode.com/users', {
+      method: 'GET',
+    });
+    const results = await response.text();
+    this.setState({ results });
+  }
+
   render() {
+    const { results } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View>
+          <TextInput
+            style={styles.preview}
+            value={results}
+            placeholder="Results..."
+            editable={false}
+            multiline
+          />
+          <TouchableOpacity onPress={this.onLoad} style={styles.button}>
+            <Text>Load data</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -14,8 +46,23 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  preview: {
+    backgroundColor: '#bdc3c7',
+    width: 300,
+    height: 400,
+    padding: 10,
+    borderRadius: 5,
+    color: '#333',
+    marginBottom: 50,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 3,
+    marginTop: 10,
   },
 });
