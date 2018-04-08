4f7a9ce386bac696cd61211317904c1836ab0e74
warlyware <danward@gmail.com>
2018-04-08 15:18:27 -0400

    Add empty ejected expokit app, add realm

--- chapter-7/realm-db/App.js
+++ chapter-7/realm-db/App.js
@@ -6,6 +6,8 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
