c4b45b3525298dc7f0bacd794c79aad74d09bfd7
warlyware <danward@gmail.com>
2018-04-08 21:02:37 -0400

    Add working recipe

--- chapter-7/realm-db/App.js
+++ chapter-7/realm-db/App.js
@@ -1,13 +1,104 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Realm from 'realm';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  realm = 'undefined';
+
+
+  getRandomUser() {
+    return fetch('https://randomuser.me/api/').then((response)=>response.json());
+  }
+
+  componentWillMount() {
+    const realm = this.realm = new Realm({
+      schema: [
+        {
+          name: 'User',
+          properties: {
+            firstName: 'string',
+            lastName: 'string',
+            email: 'string'
+          }
+        }
+      ]
+    });
+    this.setState({users:realm.objects('User')});
+  }
+
+  getRandomUser() {
+    return fetch('https://randomuser.me/api/').then((response)=>response.json());
+  }
+
+  createUser = () => {
+    const realm = this.realm;
+
+    this.getRandomUser().then((response) => {
+      const user = response.results[0];
+      const userName = user.name;
+      realm.write(() => {
+        realm.create('User', {
+          firstName: userName.first,
+          lastName: userName.last,
+          email: user.email
+        });
+        this.setState({users:realm.objects('User')});
+      });
+    });
+  }
+
+  updateUser = () => {
+    const realm = this.realm;
+    const users = realm.objects('User');
+
+    realm.write(() => {
+      if(users.length) {
+        let firstUser = users.slice(0,1)[0];
+        firstUser.firstName = 'Bob';
+        firstUser.lastName = 'Cookbook';
+        firstUser.email = 'react.native@cookbook.com';
+        this.setState(users);
+      }
+    });
+  }
+
+  deleteUsers = () => {
+    const realm = this.realm;
+    realm.write(() => {
+      realm.deleteAll();
+      this.setState({users:realm.objects('User')});
+    });
+  }
+
   render() {
+    const realm = this.realm;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.welcome}>
+          Welcome to Realm DB Test!
+        </Text>
+        <View style={styles.buttonContainer}>
+
+           <TouchableOpacity style={styles.button}
+           onPress={this.createUser}>
+            <Text style={styles.buttontext}>Add User</Text>
+          </TouchableOpacity>
+           <TouchableOpacity style={styles.button}
+            onPress={this.updateUser}>
+            <Text>Update First User</Text>
+          </TouchableOpacity>
+           <TouchableOpacity style={styles.button}
+            onPress={this.deleteUsers}>
+            <Text>Remove All Users</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+        <Text style={styles.welcome}>Users:</Text>
+        {this.state.users.map((user, idx) => {
+          return <Text key={idx}>{user.firstName} {user.lastName}
+         {user.email}</Text>;
+        })}
+        </View>
       </View>
     );
   }
@@ -16,8 +107,19 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f6f6f6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    backgroundColor: '#67CAFF',
+    padding: 15,
+    margin: 10
+  }
 });
