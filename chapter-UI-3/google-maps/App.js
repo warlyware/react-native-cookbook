1c987bb2a2ffcb5d908b8de3a52eb00eb27a3574
warlyware <danward@gmail.com>
2018-04-26 12:33:44 -0400

    Get location, render map based on recieved location

--- chapter-UI-3/google-maps/App.js
+++ chapter-UI-3/google-maps/App.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import { Permissions } from 'expo';
-import { StyleSheet, Text, View } from 'react-native';
+import { Location, Permissions, MapView } from 'expo';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default class App extends React.Component {
-  loadMap() {
+  state = {
+    location: null
+  }
 
+  async getLocation() {
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({
+      location
+    });
+    console.log(location.coords);
   }
 
   async componentDidMount() {
     const permission = await Permissions.askAsync(Permissions.LOCATION);
     if (permission.status === 'granted') {
-      this.loadMap();
+      this.getLocation();
     }
+  }
+
+  renderMap() {
+    return this.state.location ?
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: this.state.location.coords.latitude,
+          longitude: this.state.location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+      </MapView> : null
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.</Text>
+        {this.renderMap()}
       </View>
     );
   }
@@ -27,7 +49,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    flex: 1
+  }
 });
