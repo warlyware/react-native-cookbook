09042bcbbe29a5fbcad8a916d99ebdc340d16c75
warlyware <danward@gmail.com>
2018-04-28 14:38:03 -0400

    Add request for push notifications

--- chapter-UI-3/push-notifications/App.js
+++ chapter-UI-3/push-notifications/App.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Permissions, Notifications } from 'expo';
+
+const PUSH_ENDPOINT = 'https://your-server.com/users/push-token';
 
 export default class App extends React.Component {
+  registerForPushNotificationsAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    if (status !== 'granted') {
+      return;
+    }
+
+    let token = await Notifications.getExpoPushTokenAsync();
+    return fetch(PUSH_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: {
+          value: token,
+        },
+        user: {
+          username: 'Warly',
+          name: 'Dan Ward'
+        },
+      }),
+    });
+  }
+
+  componentDidMount() {
+    this.registerForPushNotificationsAsync();
+  }
+
   render() {
     return (
       <View style={styles.container}>
