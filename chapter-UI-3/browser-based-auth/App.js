533d5ca57754ccbaff5e0ff4c59de7ef568dd3f2
warlyware <danward@gmail.com>
2018-05-06 13:24:25 -0400

    Add basic working recipe

--- chapter-UI-3/browser-based-auth/App.js
+++ chapter-UI-3/browser-based-auth/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { AuthSession } from 'expo';
+
+const CLIENT_ID = 'd8e31efe1c0f46fca50bffa2d9fbd9ce';
+const REDIRECT_URI = `https://auth.expo.io/@warlyware/browser-based-auth`;
 
 export default class App extends React.Component {
+  state = {
+    result: null,
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <Button title="Login with Spotify" onPress={this._handlePressAsync} />
+        {this.state.result ? (
+          <Text>{JSON.stringify(this.state.result)}</Text>
+        ) : null}
+        </View>
     );
   }
+
+  _handlePressAsync = async () => {
+    let redirectUrl = AuthSession.getRedirectUrl();
+    let result = await AuthSession.startAsync({
+      authUrl:
+      `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}` +
+      `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
+      `&scope=${encodeURIComponent('user-read-email')}&response_type=token`
+    });
+    this.setState({ result });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
