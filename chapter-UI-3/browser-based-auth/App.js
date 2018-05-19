5ead4c0ffd0f939f4777f1d5bbcaa560544ef844
warlyware <danward@gmail.com>
2018-05-19 11:55:15 -0400

    Add remaining methods, styles

--- chapter-UI-3/browser-based-auth/App.js
+++ chapter-UI-3/browser-based-auth/App.js
@@ -1,42 +1,137 @@
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
 import { AuthSession } from 'expo';
+import { FontAwesome } from '@expo/vector-icons';
+import axios from 'axios';
 
 const CLIENT_ID = 'd8e31efe1c0f46fca50bffa2d9fbd9ce';
-const REDIRECT_URI = `https://auth.expo.io/@warlyware/browser-based-auth`;
 
-export default class App extends React.Component {
+export default class App extends Component {
   state = {
-    result: null,
+    userInfo: null,
+    didError: false
   };
+
+  handleSpotifyLogin = async () => {
+    let redirectUrl = AuthSession.getRedirectUrl();
+    let results = await AuthSession.startAsync({
+      authUrl:
+      `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}
+      &redirect_uri=${encodeURIComponent(redirectUrl)}
+      &scope=user-read-email&response_type=token`;
+    });
+    if (results.type !== 'success') {
+      console.log(results.type);
+      this.setState({ didError: true });
+    } else {
+      const userInfo = await axios.get(`https://api.spotify.com/v1/me`, {
+        headers: {
+          "Authorization": `Bearer ${results.params.access_token}`
+        }
+      });
+      this.setState({ userInfo: userInfo.data });
+    }
+  };
+
+  displayError = () => {
+    return (
+      <View style={styles.userInfo}>
+        <Text style={styles.errorText}>
+          There was an error, please try again.
+        </Text>
+      </View>
+    );
+  }
+
+  displayResults = () => {
+    { return this.state.userInfo ? (
+      <View style={styles.userInfo}>
+        <Image
+          style={styles.profileImage}
+          source={ {'uri': this.state.userInfo.images[0].url} }
+        />
+        <View>
+          <Text style={styles.userInfoText}>
+            Username:
+          </Text>
+          <Text style={styles.userInfoText}>
+            {this.state.userInfo.id}
+          </Text>
+          <Text style={styles.userInfoText}>
+            Email:
+          </Text>
+          <Text style={styles.userInfoText}>
+            {this.state.userInfo.email}
+          </Text>
+        </View>
+      </View>
+    ) : (
+      <View style={styles.userInfo}>
+        <Text style={styles.userInfoText}>
+          Login to Spotify to see user data.
+        </Text>
+      </View>
+    )}
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Button title="Login with Spotify" onPress={this._handlePressAsync} />
-        {this.state.result ? (
-          <Text>{JSON.stringify(this.state.result)}</Text>
-        ) : null}
-        </View>
+        <FontAwesome
+          name="spotify"
+          color="#2FD566"
+          size={128}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.handleSpotifyLogin}
+          disabled={this.state.userInfo ? true : false}
+        >
+          <Text style={styles.buttonText}>
+            Login with Spotify
+          </Text>
+        </TouchableOpacity>
+        {this.state.didError ?
+          this.displayError() :
+          this.displayResults()
+        }
+      </View>
     );
   }
-
-  _handlePressAsync = async () => {
-    let redirectUrl = AuthSession.getRedirectUrl();
-    let result = await AuthSession.startAsync({
-      authUrl:
-      `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}` +
-      `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
-      `&scope=${encodeURIComponent('user-read-email')}&response_type=token`
-    });
-    this.setState({ result });
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column',
+    backgroundColor: '#000',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  button: {
+    backgroundColor: '#2FD566',
+    padding: 20
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 20
+  },
+  userInfo: {
+    height: 250,
+    width: 200,
+    alignItems: 'center',
+  },
+  userInfoText: {
+    color: '#fff',
+    fontSize: 18
+  },
+  errorText: {
+    color: '#fff',
+    fontSize: 18
+  },
+  profileImage: {
+    height: 64,
+    width: 64,
+    marginBottom: 32
+  }
 });
