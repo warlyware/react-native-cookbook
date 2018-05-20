d05506a26a1c834a69915096af2a8e7a1e16bc67
warlyware <danward@gmail.com>
2018-05-20 17:41:43 -0400

    Clean up import

--- chapter-9/redux-app/App.js
+++ chapter-9/redux-app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './redux';
 
