ccb4841f59b535b27e194d35e8a9aa3285383d12
warlyware <danward@gmail.com>
2018-06-23 13:30:51 -0400

    Add working recipe

--- chapter-11/android-specific/NativeModuleApp/App.js
+++ chapter-11/android-specific/NativeModuleApp/App.js
@@ -1,39 +1,106 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
+  AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  NativeModules,
+  TextInput,
+  Switch,
+  DeviceEventEmitter,
+  TouchableWithoutFeedback
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Button from 'react-native-button';
 
-type Props = {};
-export default class App extends Component<Props> {
+const {
+  HelloManager
+} = NativeModules;
+
+export default class App extends Component {
+  componentWillMount() {
+    this.subscription = DeviceEventEmitter.addListener('GreetingResponse', (response) => {
+      this.displayResult(response.greeting);
+    });
+
+    this.setState({
+      greetingMessage: undefined
+    });
+  }
+
+  componentWillUnmount() {
+    this.subscription.remove();
+  }
+
+  displayResult = (result) => {
+    this.refs.userName.blur();
+    this.setState({ greetingMessage: result });
+  }
+
+  onBackgroundPress = () => {
+    this.refs.userName.blur();
+  }
+
+  greetUserCallback = () => {
+    const state = this.state;
+    HelloManager.greetUser(state.userName, state.isAdmin, this.displayResult);
+  }
+
+  greetUserPromise = () => {
+    const state = this.state;
+    const promise = HelloManager.greetUserWithPromises(state.userName, state.isAdmin);
+    promise.then(this.displayResult);
+  }
+
+  greetUserEvent = () => {
+    const state = this.state;
+    HelloManager.greetUserWithEvent(state.userName, !!state.isAdmin);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <TouchableWithoutFeedback onPress={this.onBackgroundPress}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.headerText}>Android Native Modules</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              ref="userName"
+              autoCorrect={false}
+              style={styles.inputField}
+              placeholder="User Name"
+              onChangeText={(text) => this.setState({ userName: text }) }
+            />
+            <Text style={styles.label}>Admin</Text>
+            <Switch style={styles.radio} onValueChange={(value) => this.setState({ isAdmin: value }) } value={this.state.isAdmin}/>
+          </View>
+          <View style={styles.flexContainer}>
+            <Button
+              containerStyle={styles.buttonContainer}
+              style={styles.buttonStyle}
+              onPress={this.greetUserCallback}>
+              Greet (callback)
+            </Button>
+            <Button
+              containerStyle={styles.buttonContainer}
+              style={styles.buttonStyle}
+              onPress={this.greetUserPromise}>
+              Greet (promise)
+            </Button>
+            <Button
+              containerStyle={styles.buttonContainer}
+              style={styles.buttonStyle}
+              onPress={this.greetUserEvent}>
+              Greet (events)
+            </Button>
+          </View>
+          <View style={styles.flexContainer}>
+            <Text>Response: </Text>
+            <Text>{this.state.greetingMessage}</Text>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
--- chapter-11/android-specific/NativeModuleApp/android/app/src/main/java/com/nativemoduleapp/HelloPackage.java
+++ chapter-11/android-specific/NativeModuleApp/android/app/src/main/java/com/nativemoduleapp/HelloPackage.java
@@ -1,51 +1,34 @@
 package com.nativemoduleapp;
 
-/**
- * Created by dan on 6/17/18.
- */
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
-import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.shell.MainReactPackage;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
 
-
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
 
+/**
+ * Created by stan229 on 7/6/16.
+ */
 public class HelloPackage implements ReactPackage {
-    @Override
-    public List
-            <NativeModule> createJSModules() {
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
     @Override
-    public List
-            <ViewManager> createViewManagers
-            (ReactApplicationContext reactContext) {
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 
     @Override
-    public List
-            <NativeModule> createNativeModules
-            (ReactApplicationContext reactContext) {
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
 
         modules.add(new HelloManager(reactContext));
 
         return modules;
-    }
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-                new MainReactPackage(),
-                new HelloPackage()
-        );
     }
 }
