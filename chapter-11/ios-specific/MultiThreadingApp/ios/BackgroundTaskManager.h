7b3643852f700f56b5d54f75331e6feb63bd0243
warlyware <danward@gmail.com>
2018-07-08 16:55:22 -0400

    Add basic iOS multithreading app

---
+++ chapter-11/ios-specific/MultiThreadingApp/ios/BackgroundTaskManager.h
@@ -0,0 +1,17 @@
+//
+//  BackgroundTaskManager.h
+//  MultiThreadingApp
+//
+//  Created by Daniel Ward on 7/8/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <dispatch/dispatch.h>
+#import "RCTBridgeModule.h"
+
+@interface BackgroundTaskManager : NSObject <RCTBridgeModule> {
+  dispatch_queue_t backgroundQueue;
+}
+
+@end
---
+++ chapter-11/ios-specific/MultiThreadingApp/ios/BackgroundTaskManager.m
@@ -0,0 +1,33 @@
+//
+//  BackgroundTaskManager.m
+//  MultiThreadingApp
+//
+//  Created by Daniel Ward on 7/8/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+#import "BackgroundTaskManager.h"
+#import "RCTBridge.h"
+#import "RCTEventDispatcher.h"
+
+@implementation BackgroundTaskManager
+
+@synthesize bridge = _bridge;
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(loadInBackground) {
+  backgroundQueue = dispatch_queue_create("com.moduscreate.bgqueue", NULL);
+  
+  dispatch_async(backgroundQueue, ^{
+    NSLog(@"processing background");
+    [self.bridge.eventDispatcher sendAppEventWithName:@"backgroundProgress" body:@{@"status": @"Loading"}];
+    [NSThread sleepForTimeInterval:5];
+    NSLog(@"slept");
+    dispatch_async(dispatch_get_main_queue(), ^{
+      NSLog(@"Done processing; main thread");
+      [self.bridge.eventDispatcher sendAppEventWithName:@"backgroundProgress" body:@{@"status": @"Done"}];
+    });
+  });
+}
+
+@end
