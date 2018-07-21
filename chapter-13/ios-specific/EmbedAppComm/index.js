fa51b8eb9f52722a2e9085f3d20f36db7a97314c
warlyware <danward@gmail.com>
2018-07-21 17:06:59 -0400

    Add ios communication recipe

---
+++ chapter-13/ios-specific/EmbedAppComm/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+class EmbedAppComm extends Component {
+  state = {
+    userName: ''
+  }
+
+  componentWillMount() {
+    this.setState({
+      userName : this.props.userName
+    });
+
+    NativeAppEventEmitter.addListener('UserNameChanged', (body) => {
+        this.setState({userName : body.userName});
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello {this.state.userName}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('EmbedAppComm', () => EmbedAppComm);
