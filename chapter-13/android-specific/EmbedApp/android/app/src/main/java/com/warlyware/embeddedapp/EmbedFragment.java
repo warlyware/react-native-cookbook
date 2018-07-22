1303e398fe8fbc05aab6646160d9ddee231caa11
warlyware <danward@gmail.com>
2018-07-22 17:52:08 -0400

    Add working embedded Android app

---
+++ chapter-13/android-specific/EmbedApp/android/app/src/main/java/com/warlyware/embeddedapp/EmbedFragment.java
@@ -0,0 +1,8 @@
+package com.warlyware.embeddedapp;
+
+public class EmbedFragment extends ReactFragment {
+  @Override
+  public String getMainComponentName() {
+    return "EmbedApp";
+  }
+}
---
+++ chapter-13/android-specific/EmbedApp/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+class EmbedApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello in React Native</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('EmbedApp', () => EmbedApp);
