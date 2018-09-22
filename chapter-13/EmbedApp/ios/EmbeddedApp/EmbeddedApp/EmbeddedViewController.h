07a636ccf84dcd5ae1beda0e6d4d032001b61e8c
warlyware <danward@gmail.com>
2018-09-22 16:44:55 -0400

    Add ViewController methods, wire buttons in native code

---
+++ chapter-13/EmbedApp/ios/EmbeddedApp/EmbeddedApp/EmbeddedViewController.h
@@ -0,0 +1,17 @@
+//
+//  EmbeddedViewController.h
+//  EmbeddedApp
+//
+//  Created by Daniel Ward on 9/22/18.
+//  Copyright © 2018 Daniel Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EmbeddedViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- chapter-13/EmbedApp/ios/EmbeddedApp/EmbeddedApp/ViewController.m
+++ chapter-13/EmbedApp/ios/EmbeddedApp/EmbeddedApp/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "EmbeddedViewController.h"
+#import <React/RCTRootView.h>
 
-@interface ViewController ()
+@interface ViewController () {
+    EmbeddedViewController *embeddedViewController;
+}
 
 @end
 
@@ -19,5 +23,42 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)openRNAppButtonPressed:(id)sender {
+    NSURL *jsCodeLocation = [NSURL
+                             URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL : jsCodeLocation
+                         moduleName        : @"EmbedNativeApp"
+                         initialProperties : nil
+                          launchOptions    : nil];
+    
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
+}
+- (IBAction)openRNAppEmbeddedButtonPressed:(id)sender {
+    NSURL *jsCodeLocation = [NSURL
+                             URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL : jsCodeLocation
+                         moduleName        : @"EmbedNativeApp"
+                         initialProperties : nil
+                          launchOptions    : nil];
+    
+    [embeddedViewController setView:rootView];
+}
+
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if([segue.identifier isEqualToString:@"embed"]) {
+        embeddedViewController = segue.destinationViewController;
+    }
+}
 
 @end
