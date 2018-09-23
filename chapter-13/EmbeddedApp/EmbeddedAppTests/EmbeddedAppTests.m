2731630b33ac2b11df0858afe5b38ab88283e81b
warlyware <danward@gmail.com>
2018-09-23 14:53:17 -0400

    Add standalone EmbeddedApp Xcode project with Podfile and Info.plist updated

---
+++ chapter-13/EmbeddedApp/EmbeddedAppTests/EmbeddedAppTests.m
@@ -0,0 +1,37 @@
+//
+//  EmbeddedAppTests.m
+//  EmbeddedAppTests
+//
+//  Created by Daniel Ward on 9/23/18.
+//  Copyright © 2018 Daniel Ward. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EmbeddedAppTests : XCTestCase
+
+@end
+
+@implementation EmbeddedAppTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
