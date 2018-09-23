b20a3e125384c370e8125e5afb70e51df0998a5c
warlyware <danward@gmail.com>
2018-09-23 14:45:51 -0400

    Add EmbeddedApp

---
+++ chapter-13/FromNativeToRN/ios/EmbeddedApp/EmbeddedApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmbeddedApp
+//
+//  Created by Daniel Ward on 9/23/18.
+//  Copyright © 2018 Daniel Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chapter-13/FromNativeToRN/ios/EmbeddedApp/EmbeddedApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  EmbeddedApp
+//
+//  Created by Daniel Ward on 9/23/18.
+//  Copyright © 2018 Daniel Ward. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
