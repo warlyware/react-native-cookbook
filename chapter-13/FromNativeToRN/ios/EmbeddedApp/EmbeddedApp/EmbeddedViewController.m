753bc5e48b96fba448eb879b218af3d07422d2fc
warlyware <danward@gmail.com>
2018-09-23 15:10:48 -0400

    Add UI elements to native project storyboard

---
+++ chapter-13/FromNativeToRN/ios/EmbeddedApp/EmbeddedApp/EmbeddedViewController.m
@@ -0,0 +1,32 @@
+//
+//  EmbeddedViewController.m
+//  EmbeddedApp
+//
+//  Created by Daniel Ward on 9/23/18.
+//  Copyright © 2018 Daniel Ward. All rights reserved.
+//
+
+#import "EmbeddedViewController.h"
+
+@interface EmbeddedViewController ()
+
+@end
+
+@implementation EmbeddedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- chapter-13/FromNativeToRN/ios/EmbeddedApp/EmbeddedApp/ViewController.m
+++ chapter-13/FromNativeToRN/ios/EmbeddedApp/EmbeddedApp/ViewController.m
@@ -7,17 +7,65 @@
 //
 
 #import "ViewController.h"
+#import <React/RCTRootView.h>
+#import <React/RCTBridge.h>
+#import <React/RCTEventDispatcher.h>
+#import "EmbeddedViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <RCTBridgeDelegate> {
+    EmbeddedViewController *embeddedViewController;
+    RCTBridge *_bridge;
+    BOOL isRNRunning;
+}
+
+@property (weak, nonatomic) IBOutlet UITextField *userNameField;
 
 @end
 
 @implementation ViewController
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
+    NSURL *jsCodeLocation = [NSURL
+                             URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+    return jsCodeLocation;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
 
+- (IBAction)openRNAppEmbeddedButtonPressed:(id)sender {
+    NSString *userName = _userNameField.text;
+    NSDictionary *props = @{@"userName" : userName};
+    
+    if(_bridge == nil) {
+        _bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:nil];
+    }
+    
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBridge :_bridge
+                             moduleName : @"EmbedAppComm"
+                      initialProperties : props];
+    
+    isRNRunning = true;
+    [embeddedViewController setView:rootView];
+}
+
+- (IBAction)onUserNameChanged:(id)sender {
+    if(isRNRunning == YES && _userNameField.text.length > 3) {
+        [_bridge.eventDispatcher sendAppEventWithName:@"UserNameChanged" body:@{@"userName" : _userNameField.text}];
+    }
+}
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if([segue.identifier isEqualToString:@"embed"]) {
+        embeddedViewController = segue.destinationViewController;
+    }
+}
 @end
